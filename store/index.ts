135ad3996f7a2d465932842ca6c2e79f99f4e408
trastanechora <trastanechora@gmail.com>
2020-07-21 15:19:59 +0700

    🗄  - Added vuex store

---
+++ store/index.ts
@@ -0,0 +1,28 @@
+/*
+ * This is the root of store which are other files as vuex store module
+ * define related state unique name in each files
+ *
+ * https://nuxtjs.org/guide/vuex-store#modules-mode
+ */
+
+// The highest-performing engineers are 1.5 times more likely to have easy-to-use tools.”
+// - State of DevOps 2019 Summit
+
+// state defined as function
+export const state = () => ({
+  pageTitle: ''
+})
+
+// mutations defined as object
+export const mutations = {
+  setPageTitle(store: any, param: string) {
+    store.pageTitle = param
+  }
+}
+
+// this is root of store, mutations defined must unique
+export const actions = {
+  setPageTitle(state: any, param: string) {
+    state.commit('setPageTitle', param)
+  }
+}
